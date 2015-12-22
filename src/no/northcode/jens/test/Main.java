372cfe3fa71b17cd5852e6a4c45690510bb6f551
Jens <jens@northcode.no>
2015-12-22 23:56:39 +0100

    Testing...

--- src/no/northcode/jens/test/Main.java
+++ src/no/northcode/jens/test/Main.java
@@ -1,7 +1,9 @@
 package no.northcode.jens.test;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import no.northcode.jens.intranetsek2.Lesson;
 import no.northcode.jens.intranetsek2.Login;
 import no.northcode.jens.intranetsek2.News;
 import no.northcode.jens.intranetsek2.School;
@@ -13,13 +15,16 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception
 	{
-		Login l = new Login("jens.vogler", "-", "kho");
-		List<News> news = l.fetchNews();
-		for(News n : news) {
-			System.out.println(n.text);
+		List<School> schools = School.getSchoolList();
+		for(School s : schools) {
+			System.out.println(s.getName());
 		}
-		String result = l.postRequest("https://intranet.tam.ch/kho/timetable/ajax-get-timetable", "startDate=1450047600000&endDate=1450566000000&studentId%5B%5D=4014351&holidaysOnly=0");
-		System.out.println(result);
 		
+		
+		Login l = new Login("jens.vogler", "-", "kho");
+		List<Lesson> lessons = Lesson.getLessonByDay(l, LocalDate.parse("2015-12-15"));
+		for(Lesson les : lessons) {
+			System.out.println(les.toString());
+		}
 	}
 }
